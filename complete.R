b961fe7a1e6e11e5c8a32283aac3eb729898a1ae
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-03-18 00:27:00 +0530

    use <- instead of = and, remove .Rdata/.Rhistory and add .gitignore

--- complete.R
+++ complete.R
@@ -1,19 +1,19 @@
 complete <- function(directory, id = 1:332) {
-    monitors = list.files(directory) 
-    subsetOfMonitors = monitors[id]
+    monitors <- list.files(directory) 
+    subsetOfMonitors <- monitors[id]
 
-    dataFrame = data.frame(id = numeric(0), nobs = numeric(0))
-    colnames(dataFrame) = c("id", "nobs")
+    dataFrame <- data.frame(id = numeric(0), nobs = numeric(0))
+    colnames(dataFrame) <- c("id", "nobs")
     for (index in 1:length(id)) {
-        monitor = subsetOfMonitors[index]
-        monitorId = id[index]
-        monitorFile = file.path(directory, monitor)
-        monitorData = read.csv(monitorFile, header = TRUE)
+        monitor <- subsetOfMonitors[index]
+        monitorId <- id[index]
+        monitorFile <- file.path(directory, monitor)
+        monitorData <- read.csv(monitorFile, header = TRUE)
 
-        sulfateObservations = sum(!is.na(monitorData$sulfate))
-        nitrateObservations = sum(!is.na(monitorData$nitrate))
-        completeObservations = min(sulfateObservations, nitrateObservations)
-        dataFrame[index,] = c(monitorId, completeObservations)
+        sulfateObservations <- sum(!is.na(monitorData$sulfate))
+        nitrateObservations <- sum(!is.na(monitorData$nitrate))
+        completeObservations <- min(sulfateObservations, nitrateObservations)
+        dataFrame[index,] <- c(monitorId, completeObservations)
     }
 
     dataFrame
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,12 +1,12 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-    monitors = list.files(directory)
-    subsetOfMonitors = monitors[id]
-    pollutantData = numeric(0)
+    monitors <- list.files(directory)
+    subsetOfMonitors <- monitors[id]
+    pollutantData <- numeric(0)
 
     for (monitor in subsetOfMonitors) {
-        monitorFile = file.path(directory, monitor)
-        monitorData = read.csv(monitorFile, header = TRUE)
-        pollutantData = c(pollutantData, monitorData[[pollutant]])
+        monitorFile <- file.path(directory, monitor)
+        monitorData <- read.csv(monitorFile, header = TRUE)
+        pollutantData <- c(pollutantData, monitorData[[pollutant]])
     }
 
     mean(pollutantData, na.rm = TRUE)
