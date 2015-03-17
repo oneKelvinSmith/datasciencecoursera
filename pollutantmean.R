03b3400c430557c6f3bd7cd7e92d3a5e15a3047b
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-03-17 22:10:23 +0530

    refactor pollutantmean and comment out examples

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,17 +1,17 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  monitors = list.files(directory) 
-  subsetOfMonitors = monitors[id]
-  pollutantData = numeric(0)
+    monitors = list.files(directory)
+    subsetOfMonitors = monitors[id]
+    pollutantData = numeric(0)
 
-  for (monitor in subsetOfMonitors) {
-    file = file.path(directory, monitor)
-    monitorData = read.csv(file, header = TRUE)
-    pollutantData = c(pollutantData, monitorData[[pollutant]])
-  }
+    for (monitor in subsetOfMonitors) {
+        monitorFile = file.path(directory, monitor)
+        monitorData = read.csv(monitorFile, header = TRUE)
+        pollutantData = c(pollutantData, monitorData[[pollutant]])
+    }
 
-  mean(pollutantData, na.rm = TRUE) 
+    mean(pollutantData, na.rm = TRUE)
 }
 
-pollutantmean("specdata", "sulfate", 1:10)
-pollutantmean("specdata", "nitrate", 70:72)
-pollutantmean("specdata", "nitrate", 23)
+# pollutantmean("specdata", "sulfate", 1:10)
+# pollutantmean("specdata", "nitrate", 70:72)
+# pollutantmean("specdata", "nitrate", 23)
